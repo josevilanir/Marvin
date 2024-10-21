a5fe3703a30d0e5d08b7377d0ac751e4d22dbb0a
jose.brito <jose.brito@dataprev.gov.br>
2024-10-21 00:19:48 -0300

    Testes implementados sobre a função de pausar musica

--- tests/tests.py
+++ tests/tests.py
@@ -1,24 +1,24 @@
 import pytest
-from unittest.mock import patch, MagicMock
-from comandos.tocar_musica import pausar_musica, listar_dispositivos_spotify
+from unittest.mock import patch
+from comandos.tocar_musica import pausar_musica  
 
 def test_pausar_musica_sem_dispositivo():
-    with patch('comandos.listar_dispositivos_spotify', return_value=[]):
+    with patch('comandos.tocar_musica.listar_dispositivos_spotify', return_value=[]):
         with pytest.raises(Exception) as excinfo:
             pausar_musica()
         assert str(excinfo.value) == "Nenhum dispositivo disponível. Por favor, verifique se há dispositivos conectados."
 
 def test_pausar_musica_sucesso():
-    with patch('comandos.listar_dispositivos_spotify', return_value=['Dispositivo 1']), \
-         patch('seu_modulo.sp.pause_playback') as mock_pause:
+    with patch('comandos.tocar_musica.listar_dispositivos_spotify', return_value=['Dispositivo 1']), \
+         patch('comandos.tocar_musica.sp.pause_playback') as mock_pause:
         
         pausar_musica()
-        mock_pause.assert_called_once()  # Verifica se a função pause_playback foi chamada uma vez
+        mock_pause.assert_called_once()
 
 def test_pausar_musica_erro():
-    with patch('comandos.listar_dispositivos_spotify', return_value=['Dispositivo 1']), \
-         patch('comandos.sp.pause_playback', side_effect=Exception("Erro ao pausar")):
+    with patch('comandos.tocar_musica.listar_dispositivos_spotify', return_value=['Dispositivo 1']), \
+         patch('comandos.tocar_musica.sp.pause_playback', side_effect=Exception("Erro ao pausar")):
         
         with pytest.raises(Exception) as excinfo:
             pausar_musica()
-        assert str(excinfo.value) == "Erro ao pausar"  # Verifica se a exceção correta é levantada
+        assert str(excinfo.value) == "Erro ao pausar"
