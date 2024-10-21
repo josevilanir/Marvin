fc17fad57488fe321a19acebc1bd3a0c145951c8
jose.brito <jose.brito@dataprev.gov.br>
2024-10-21 00:02:04 -0300

    Correção em testes

--- tests/tests.py
+++ tests/tests.py
@@ -1,23 +1,23 @@
 import pytest
 from unittest.mock import patch, MagicMock
-from comandos.tocar_musica import *  
+from comandos.tocar_musica import pausar_musica, listar_dispositivos_spotify
 
 def test_pausar_musica_sem_dispositivo():
-    with patch('seu_modulo.listar_dispositivos_spotify', return_value=[]):
+    with patch('comandos.listar_dispositivos_spotify', return_value=[]):
         with pytest.raises(Exception) as excinfo:
             pausar_musica()
         assert str(excinfo.value) == "Nenhum dispositivo disponível. Por favor, verifique se há dispositivos conectados."
 
 def test_pausar_musica_sucesso():
-    with patch('seu_modulo.listar_dispositivos_spotify', return_value=['Dispositivo 1']), \
+    with patch('comandos.listar_dispositivos_spotify', return_value=['Dispositivo 1']), \
          patch('seu_modulo.sp.pause_playback') as mock_pause:
         
         pausar_musica()
         mock_pause.assert_called_once()  # Verifica se a função pause_playback foi chamada uma vez
 
 def test_pausar_musica_erro():
-    with patch('seu_modulo.listar_dispositivos_spotify', return_value=['Dispositivo 1']), \
-         patch('seu_modulo.sp.pause_playback', side_effect=Exception("Erro ao pausar")):
+    with patch('comandos.listar_dispositivos_spotify', return_value=['Dispositivo 1']), \
+         patch('comandos.sp.pause_playback', side_effect=Exception("Erro ao pausar")):
         
         with pytest.raises(Exception) as excinfo:
             pausar_musica()
